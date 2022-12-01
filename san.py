2b49c91b2a77ebf8d96191fa03be6f5064e83fa6
Peter McGarrigle <mcgarrigle@github.com>
2022-12-01 21:59:47 +0000

    san works with list returns full SAN class instance

--- san.py
+++ san.py
@@ -6,15 +6,19 @@ class SAN:
     def __init__(self, san):
         self.san = san
 
-    @property
-    def value(self):
-        (label, subject) = self.san.split(":")
+    def item(self, name):
+        (label, subject) = name.split(":")
         if label == "DNS":
             return x509.DNSName(subject)
         if label == "IP":
             return x509.IPAddress(ipaddress.IPv4Address(subject))
-        raise RuntimeError(f"SAN type for {self.san} not known")
+        raise RuntimeError(f"SAN type for {name} not known")
 
-for subject in [ "DNS:www.cryptography.io", "IP:192.168.1.1" ]:
-  dn = SAN(subject)
-  print(dn.value)
+    @property
+    def value(self):
+        items = [ self.item(element) for element in self.san ]
+        return x509.SubjectAlternativeName(items)
+
+subjects = [ "DNS:www.cryptography.io", "IP:192.168.1.1" ]
+san = SAN(subjects)
+print(san.value)
