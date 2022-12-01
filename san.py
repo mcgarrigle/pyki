cdb8bfa192740306e94841affdf97729de90a4ad
Peter McGarrigle <mcgarrigle@github.com>
2022-12-01 19:40:24 +0000

    san working

--- san.py
+++ san.py
@@ -1,18 +1,13 @@
-
-from cryptography import x509
 import ipaddress
-
-san = x509.SubjectAlternativeName(
-        [ x509.DNSName(u'cryptography.io'), x509.DNSName(u'www.cryptography.io'), x509.IPAddress(ipaddress.IPv4Address('192.168.1.1')) ]
-    )
-
+from cryptography import x509
 
 class SAN:
 
     def __init__(self, san):
         self.san = san
 
-    def entry(self):
+    @property
+    def value(self):
         (label, subject) = self.san.split(":")
         if label == "DNS":
             return x509.DNSName(subject)
@@ -20,13 +15,6 @@ class SAN:
             return x509.IPAddress(ipaddress.IPv4Address(subject))
         raise RuntimeError(f"SAN type for {self.san} not known")
 
-#builder = builder.subject_name(x509.Name([
-#    x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
-#]))
-
-#dn=[ x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography'), x509.NameAttribute(NameOID.DOMAIN_COMPONENT, u'example') , x509.NameAttribute(NameOID.DOMAIN_COMPONENT, u'com') ]
-#print(dn)
-
 for subject in [ "DNS:www.cryptography.io", "IP:192.168.1.1" ]:
   dn = SAN(subject)
-  print(dn.entry())
+  print(dn.value)
