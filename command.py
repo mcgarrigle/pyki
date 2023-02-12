e258165b2cacfbd8e008deb461b88d33e255bb96
Peter McGarrigle <mcgarrigle@github.com>
2023-02-12 22:02:48 +0000

    do not include SAN extension if the are not SAN supplied

--- command.py
+++ command.py
@@ -74,14 +74,17 @@ class Command:
     # Namespace(command='cert', dn='CN=X', key='www.key', cert='www.crt', ca_key='ca.key', ca_cert='ca.crt', san=[])
 
     def cert(self, args):
-        san = SAN(args.san)
         ca_key = Key.load(args.ca_key)
         ca_cert = Certificate.load(args.ca_cert)
         key = Key.new(args.key)
         basic = x509.BasicConstraints(ca=False, path_length=None) 
         usage = self.key_usage(digital_signature=True, key_encipherment=True)
         usages = x509.ExtendedKeyUsage([ x509.oid.ExtendedKeyUsageOID.SERVER_AUTH, x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH ])
-        extensions = [ (basic, False), (usage, True), (usages, False), (san.value, False) ]
+        if args.san:
+            san = SAN(args.san)
+            extensions = [ (basic, False), (usage, True), (usages, False), (san.value, False) ]
+        else:
+            extensions = [ (basic, False), (usage, True), (usages, False) ]
         cert = Certificate.create(args.dn, key, extensions)
         cert.sign(ca_cert.issuer, ca_key, args.expires)
         cert.save(args.cert)
